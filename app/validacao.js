a0d6f66b04ec0a2a06598c524c3716d2acba16cf
Daniel Miranda <daniel.danjos@gmail.com>
2023-03-24 03:23:49 -0600

    Add files via upload

--- app/validacao.js
+++ app/validacao.js
@@ -1,6 +1,21 @@
 function verificaSeoChutePossuiUmValorValido(chute) { 
     const numero = +chute;
 
+    if (chuteForInvalido(numero)) {
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
+    }
 
     if (chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div>Valor inválido</div>';
